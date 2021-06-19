acccf07a95661712fde0f2957b8d6d04d211e3aa
Demetrius <demetrius.mp789@gmail.com>
2021-06-18 22:15:38 -0400

    Arquivo de execução das meta heurísticas

---
+++ run.py
@@ -0,0 +1,22 @@
+from hill_climbing.hill_climbing import HillClimbing
+from simulated_annealing.simulated_annealing import SimulatedAnnealing
+from utils import Grafo
+import time
+
+if __name__ == '__main__':
+    entrada = 'dados/att48.tsp.txt'
+    grafo = Grafo.gerar_grafo(entrada)
+
+    hc = HillClimbing(grafo, max_iter=1000)
+    inicio = round(time.time() * 1000)
+    _, distancia = hc.run()
+    print(f'Tempo do Hill Climbing: {round(time.time() * 1000) - inicio}')
+    print(distancia)
+
+    print('='*30)
+
+    sa = SimulatedAnnealing(grafo, t_max=10**10, tx_resfria=0.995, t_min=1e-15, max_iter=10**10)
+    inicio = round(time.time() * 1000)
+    _, distancia = sa.run()
+    print(f'Tempo do Simulated Annealing: {round(time.time() * 1000) - inicio}')
+    print(distancia)
