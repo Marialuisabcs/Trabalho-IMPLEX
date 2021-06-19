25bac757182f773ffd26493b7e47468d039bfa95
Demetrius <demetrius.mp789@gmail.com>
2021-06-19 03:28:09 -0400

    Iniciando CLI

--- run.py
+++ run.py
@@ -1,8 +1,61 @@
 from metaheuristicas import HillClimbing, SimulatedAnnealing
 from utils import Grafo
 import time
+import os
+
+
+def mostra_metaheuristicas():
+    print('Metaheurísticas:')
+    print('\t1. Simulated Annealing')
+    print('\t2. Hill Climbing')
+    print()
+
+
+def mostra_arquivos():
+    arquivos = os.listdir('dados')
+    for idx, arquivo in enumerate(arquivos):
+        if idx % 3 == 0 and idx != 0:
+            print()
+        if idx == 6:
+            print(f'{idx}. {arquivo}', end='\t\t')
+            continue
+
+        print(f'{idx}. {arquivo}', end='\t\t\t')
+
+    return arquivos
+
+
+def simulated_annealing():
+    print()
+    print('\t\t\t\t  ===== Simulated Annealing =====')
+    arquivos = mostra_arquivos()
+    print('\n')
+    arquivo = input('Digite o número correspondente ao arquivo desejado: ')
+    if arquivo not in list(range(len(arquivos))):
+        arquivo = input('Por favor, digite uma opção válida: ')
+
+
+def hill_climbing():
+    pass
+
+
+def main():
+    print('=== Trabalho final - Implementação algorítmica ===')
+    mostra_metaheuristicas()
+    option = input('Digite o número referente a metaheurística desejada: ')
+    while option not in ('1', '2'):
+        option = input('Por favor, digite uma opção válida: ')
+
+    if option == '1':
+        simulated_annealing()
+
+    if option == '2':
+        hill_climbing()
+
 
 if __name__ == '__main__':
+    main()
+    '''
     grafo = Grafo('dados/berlin52.tsp.txt')
     grafo.ler_vertices()
 
@@ -21,3 +74,4 @@ if __name__ == '__main__':
     print(f'Tempo do Simulated Annealing: {round(time.time() * 1000) - inicio}')
     print(distancia)
     sa.grafo.desenhar_solucao('SA', save=True)
+    '''
